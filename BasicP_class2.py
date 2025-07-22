251eff6c9cdb4d4e45d59ebf82761edeef558a38
Student <Student@Train11-3-PC204.control.ad.sit.kmutt.ac.th>
2025-07-22 16:15:06 +0700

    BasicProgramming If-else

---
+++ BasicP_class2.py
@@ -0,0 +1,38 @@
+# Title : Variable
+# x = 10
+# y = type(x)
+# z = True
+# j = 99.99999
+# i = str(y) + ' ' + str(z)
+# print(i , int(z) , type(j))
+
+# Title : Input
+# x = input("Type Somthing : ")
+# y = int(input("Type Number : "))
+# print("1. Your Ans : " + x + " " + str(y) + '\n' + "2. Your Ans Type : " + str(type(x)) + " " + str(type(y)))
+
+# Title : If-else
+# x = 10
+# y = 20
+# if x == y :
+#     print("1.เหมือนกันอยู่นะ")
+# else :
+#     print("1.ไม่เห็นเหมือนกันเลย")
+#     y = int(input("พิมพ์ค่า y ใหม่ : "))
+
+# if x == y :
+#     print("2.ค่าเท่ากันและ")
+# elif x <= y:
+#     print("2.ค่า y เยอะไปหน่อยนะ")
+# else :
+#     print("2.ค่า y น้อยไปหน่อยนะ")
+
+# x = 10
+# y = 20
+# i = 0
+
+# while x != y :
+#     i += 1
+#     print('\n' + str(i) + "." + "เลขยังไม่เท่ากันนะ")
+#     x = int(input("Type Number : "))
+# print('\n' + "เก่งมาก ถูกต้องละคร้าบ")
---
+++ BasicP_class2_workshop.py
@@ -0,0 +1,30 @@
+x = int(input("กรอกระยะทางเป็นจำนวนเต็ม : "))
+
+if x < 0 :
+    print("ไม่ตลก อย่าชีวิตติดลบ")
+    x = x * -1
+    print("ระยะทางใหม่ : " + str(x) + '\n')
+elif x == 0 :
+    print("เดินมาก็ได้มั้ง" + '\n')
+
+def func() :
+    print("ระยะทาง : " + str(x))
+
+if 5 <= x <= 50 :
+    func()
+    print("ค่าส่ง 10 บาท")
+elif 51 <= x <= 100 :
+    func()
+    print("ค่าส่ง 15 บาท")
+elif 101 <= x <= 300 :
+    func()
+    print("ค่าส่ง 25 บาท")
+elif 301 <= x <= 500 :
+    func()
+    print("ค่าส่ง 35 บาท")
+elif 501 <= x :
+    func()
+    print("ค่าส่ง 45 บาท")
+else :
+    func()
+    print("ส่งฟรีใส่Code : IHAVECPU ด้วยนะจ๊ะ")
